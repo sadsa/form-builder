2b6f6056828a1ee9ff070def455ac0e021f8db2f
Joshua Biddick <joshua.biddick@cucumber.co.nz>
2017-11-10 13:07:36 +1300

    Add bootstrapCSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  test: number = 12;
   render() {
     return (
       <div className="App">
